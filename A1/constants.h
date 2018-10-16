a2b25b20ce00ce8d83bb37b1df40784ce8287102
Ángel <angel7delcastillo@gmail.com>
2018-10-16 20:53:51 +0200

    removed client ports from constants.h

--- A1/constants.h
+++ A1/constants.h
@@ -1,9 +1,6 @@
 #define PORT_SERVER1 50000 //Private port to avoid potential conflicts
 #define PORT_SERVER2 50001
 #define PORT_SERVER3 50002
-#define PORT_CLIENT1 50011 //Different to server's in case they both run on the same machine
-#define PORT_CLIENT2 50012
-#define PORT_CLIENT3 50013
 #define MSGLEN 2 //2 numbers
 #define RESLEN 6 //In case of division, 3 decimals: x.xxx\0
 #define SERVER_ADDRESS "127.0.0.1" //Change if server runs on a different machine

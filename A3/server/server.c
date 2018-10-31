645fed405b83d9d44f55fa00f78234cda90ad60c
Ángel <angel7delcastillo@gmail.com>
2018-10-31 19:35:14 +0100

    server A3 v1

--- A3/server/server.c
+++ A3/server/server.c
@@ -2,16 +2,26 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <strings.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <pthread.h>
 #include "../constants.h"
+
+
+/* Structure to pass as argument to the connection handler */
+typedef struct pthread_arg_t {
+    int fd; //File descriptor for the socket
+    struct sockaddr_in address; //Client address
+} pthread_arg_t;
+
 
 /* Function which creates the TCP socket pointed by  sock, binds it to the address
    pointed by sock_address using the port port and starts listening) */
 int create_bind_listen_socket(int *sock, struct sockaddr_in *sock_address, int port){
-    //Create UDP socket
+    //Create TCP socket
     bzero(sock_address, sizeof(struct sockaddr_in));
     sock_address->sin_family=AF_INET;
     sock_address->sin_addr.s_addr=INADDR_ANY;
@@ -40,134 +50,130 @@ int create_bind_listen_socket(int *sock, struct sockaddr_in *sock_address, int p
     
     return 0;
 }
+
+
+/* TODO: Thread function */
+void *connection_handler(void *arg){
+    char op, read, aux;
+    char buf[MSGLEN], auxbuf[MSGLEN], result_msg[RESLEN];
+    double num1, num2, result;
+    int i, fd;
+    pthread_arg_t *thread_arg;
+
+    thread_arg = (pthread_arg_t *) arg;
+    fd = thread_arg->fd;
+
+    //Receive message with numbers and operation
+    if((aux = recv(fd, (char *)&buf, MSGLEN, 0)) == -1){
+        fprintf(stderr, "SERVER: Error when receiving message from client.\n");
+        close(fd);
+        return NULL;
+    }
+    else if (aux == 0) { //Client closed connection
+        printf("SERVER: Client closed the connection.\n");
+        return NULL;
+    }
+ 
+    //Get numbers and operation. No need to check the message's format: clients do that
+    //First number
+    i = 0;
+    while(buf[i] != '+' && buf[i] != '-' && buf[i] != '*' && buf[i] != '/'){
+        read = buf[i];
+        auxbuf[i] = read;
+        i++;
+    }
+    auxbuf[i] = '\0'; //End of string
+    //String to double
+    num1 = strtod(auxbuf, NULL);
+
+    //Operation:
+    op = buf[i];
+    i++;
+
+    //Second number
+    num2 = strtod(&buf[i], NULL);
+
+    //Result
+    switch(op){
+    case '+' :
+        result = num1+num2;
+        break;
+    case '-' :
+        result = num1-num2;
+        break;
+    case '*' :
+        result = num1*num2;
+        break;
+    case '/': 
+        result = num1/num2;
+    }
+       
+    //Create a message with the result
+    memset(result_msg, 0, sizeof(result_msg));
+    snprintf((char*)&result_msg, RESLEN, "%.3f", result);
+        
+    //Send the result
+    if(send(fd, &result_msg, RESLEN, 0) == -1){
+        fprintf(stderr, "SERVER: Error when trying to send the result.\n");
+        close(fd);
+        exit(1);
+    }
+
+    close(fd);
+
+    return NULL;
+}
     
 
 int main(){
 
-    int sock1, sock2, sock3, sock1_client, sock2_client, sock3_client, num1, num2, aux;
-    char buf[MSGLEN], op;
-    double result; //Double and not int for division
-    char result_msg[RESLEN];
+    int listening_sock;
     socklen_t addrlen;
-    struct sockaddr_in sock_address1, sock_address2, sock_address3;
+    struct sockaddr_in lsock_address;
+    pthread_t thread;
+    pthread_arg_t *thread_arg;
+    pthread_attr_t thread_attr;
 
     addrlen = sizeof(struct sockaddr_in);
     
-    //Socket server <-> client 1
-    if(create_bind_listen_socket(&sock1, &sock_address1, PORT_SERVER1) != 0){
-        fprintf(stderr, "SERVER: couldn't create socket for client 1.\n");
+    //Listening socket
+    if(create_bind_listen_socket(&listening_sock, &lsock_address, PORT_SERVER) != 0){
+        fprintf(stderr, "SERVER: couldn't create listening socket.\n");
         return 1;
     }
 
-    //Socket server <-> client 2
-    if(create_bind_listen_socket(&sock2, &sock_address2, PORT_SERVER2) != 0){
-        fprintf(stderr, "SERVER: couldn't create socket for client 2.\n");
-        return 1;
+    //Initialise thread attributes
+    if(pthread_attr_init(&thread_attr) != 0){
+        fprintf(stderr, "Error initialising thread attributes.\n");
+        exit(1);
+    }
+    if(pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_DETACHED) != 0){
+        fprintf(stderr, "Error pthread_attr_setdetachstate.\n");
+        exit(1);
     }
 
-    //Socket server <-> client 3
-    if(create_bind_listen_socket(&sock3, &sock_address3, PORT_SERVER3) != 0){
-        fprintf(stderr, "SERVER: couldn't create socket for client 3.\n");
-        return 1;
-    }
 
-    
     //Server loop
     while(1){
-        //Receive numbers from client 1
-        printf("SERVER: waiting for 2 digits.\n");
-
-        if((sock1_client = accept(sock1, (struct sockaddr *) &sock_address1, &addrlen)) < 0){
-            fprintf(stderr, "SERVER: Error when accepting connetion from client 1.\n");
-            close(sock1);
-            close(sock2);
-            close(sock3);            
-            exit(1);
+        //Structure to pass to the connection handler
+        if((thread_arg = (pthread_arg_t *)malloc(sizeof(pthread_arg_t))) == NULL){
+            fprintf(stderr, "Error at malloc.\n");
+            continue;
         }
-            
-
-        if((aux = recv(sock1_client, (char *)&buf, MSGLEN, 0)) == -1){
-            fprintf(stderr, "SERVER: Error when receiving message with 2 digits.\n");
-            close(sock1);
-            close(sock2);
-            close(sock3);            
-            exit(1);
-        }
-        else if (aux == 0) { //Client closed connection
-            printf("SERVER: Client 1 closed the connection.\n");
-            //            close(sock1);
-        }
-        printf("SERVER: digits received, waiting for operation\n");
-
-        //Accept connection from client 2 and get the operation
-        if((sock2_client = accept(sock2, (struct sockaddr *) &sock_address2, &addrlen)) < 0){
-            fprintf(stderr, "SERVER: Error when accepting connetion from client 2.\n");
-            close(sock1);
-            close(sock2);
-            close(sock3);            
-            exit(1);
-        }
-
-        if((aux = recv(sock2_client, (char *)&op, 1, 0)) == -1){
-            fprintf(stderr, "SERVER: Error when receiving message with operation.\n");
-            close(sock1);
-            close(sock2);
-            close(sock3);            
-            exit(1);
-        }
-        else if (aux == 0) { //Client closed connection
-            printf("SERVER: Client 1 closed the connection.\n");
-            //            close(sock2);
-        }
-
- 
-        printf("SERVER: operation received.\n");
         
-
-        //Get numbers and operation. No need to check the message's format: clients do that
-        num1 = buf[0] - '0';
-        num2 = buf[1] - '0';
-        switch(op){
-        case '+' :
-            result = num1+num2;
-            break;
-        case '-' :
-            result = num1-num2;
-            break;
-        case '*' :
-            result = num1*num2;
-            break;
-        case '/': 
-            result = (double)num1/(double)num2;
-        }
-       
-        //Create a message with the result
-        snprintf((char*)&result_msg, RESLEN, "%.3f",result);
-        
-        
-        //Accept connection from client 3 and send him the result
-        if((sock3_client = accept(sock3, (struct sockaddr *) &sock_address3, &addrlen)) < 0){
-            fprintf(stderr, "SERVER: Error when accepting connetion from client 3.\n");
-            close(sock1);
-            close(sock2);
-            close(sock3);            
-            exit(1);
+        //When accepting a connection, create a new thread for that client
+        if((thread_arg->fd = accept(listening_sock, (struct sockaddr *)&thread_arg->address, &addrlen)) != 0){
+            fprintf(stderr, "Error when accepting connection.\n");
+            free(thread_arg);
+            continue;
         }
 
-       printf("SERVER: sending result.\n");
-
-       if(write(sock3_client, &result_msg, RESLEN) == -1){
-           fprintf(stderr, "SERVER: Error when trying to send the result.\n");
-           close(sock1);
-           close(sock2);
-           close(sock3);
-           exit(1);
+        if((pthread_create(&thread, &thread_attr, connection_handler, (void *)thread_arg)) != 0){
+            fprintf(stderr, "Error when creating new thread.\n");
+            free(thread_arg);
+            continue;
         }
-       printf("SERVER: result sent.\n");
     }
 
-    /* The sockets are not closed explicitly. However, I've checked with netstat --listen and
-       SIGINT (ctrl-c) does close it */
-    
+    return 0;
 }
-            

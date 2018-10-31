a3314584936915a82b1009046b7a3bd6a63e98ce
Ángel <angel7delcastillo@gmail.com>
2018-10-31 19:38:45 +0100

    modified constants

--- A3/constants.h
+++ A3/constants.h
@@ -1,7 +1,5 @@
-#define PORT_SERVER1 50000 //Private port to avoid potential conflicts
-#define PORT_SERVER2 50001
-#define PORT_SERVER3 50002
-#define MSGLEN 2 //2 numbers
-#define RESLEN 6 //In case of division, 3 decimals: x.xxx\0
+#define PORT_SERVER 50000 //Private port to avoid potential conflicts
+#define MSGLEN 15 //max 7 chars/number
+#define RESLEN 10
 #define SERVER_ADDRESS "127.0.0.1" //Change if server runs on a different machine
 #define LOCALHOST "127.0.0.1"
